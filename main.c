872f9385096b45d91ebe408b8c20fc161f50eb9e
Caio Madeira <caiomadeiracontato@outlook.com>
2024-03-12 00:22:29 -0300

    commit files to use in WSL and change toolchain version

--- main.c
+++ main.c
@@ -13,6 +13,10 @@
 #include <pspgu.h>
 #include <pspgum.h>
 
+// Built-in Libraries
+#include <string.h>
+#include <malloc.h>
+
 // Wrapper called gu2gl.h
 // This essentially convert the GU functions to OPENGL functions implementations
 // with this the functions get more simpler to implement
@@ -44,7 +48,7 @@ static unsigned int __attribute__((aligned(16))) list[262144];
 //  A vertice is a single point in a geometric shape
 struct Vertex
 {
-    float u, v // texture coords
+    float u, v; // texture coords
     unsigned int color;
     float x, y, z;
 };
@@ -77,30 +81,123 @@ void reset_transform(float x, float y, float z)
 }
 
 // TEXTURES HELP FUNCTIONS
-unsigned int pow2(const unsigned int value)
-{
-    unsigned int power_of_two = 1/
-    while(power_of_two < value)
-    {
-        power_of_two <<= 1;
+/************************************************************************
+swizzle_fast
+Separe the TEXTURE INTO blocks and copy these blocks in different blocks
+reducing the amount of cache.
+
+swillizing is very complicated and I recommend studying more.
+
+The overall of implement this function is that the texture throughput 
+(rendimento de textura) is way way higher if you have swizzling enabled.
+/************************************************************************/
+void swizzle_fast(u8 *out, const u8 *in, const unsigned int width, const unsigned int height) {
+    unsigned int blockx, blocky;
+    unsigned int j;
+
+    unsigned int width_blocks = (width / 16);
+    unsigned int height_blocks = (height / 8);
+
+    unsigned int src_pitch = (width - 16) / 4;
+    unsigned int src_row = width * 8;
+
+    const u8 *ysrc = in;
+    u32 *dst = (u32 *)out;
+
+    for (blocky = 0; blocky < height_blocks; ++blocky) {
+        const u8 *xsrc = ysrc;
+        for (blockx = 0; blockx < width_blocks; ++blockx) {
+            const u32 *src = (u32 *)xsrc;
+            for (j = 0; j < 8; ++j) {
+                *(dst++) = *(src++);
+                *(dst++) = *(src++);
+                *(dst++) = *(src++);
+                *(dst++) = *(src++);
+                src += src_pitch;
+            }
+            xsrc += 16;
+        }
+        ysrc += src_row;
     }
-    return power_of_two/
 }
 
-void copy_texture_data(void* dest, const void* src, const in pW, const int width, const int height)
-{
-    for(unsigned int y = 0; y < height; y++)
-    {
-        for(unsigned int y = 0; x < width; y++)
-        {
-            ((unsigned int*)dest)[x + y * pW] = ((unsigned int*)src)[x + y * width];
+unsigned int pow2(const unsigned int value) {
+    unsigned int poweroftwo = 1;
+    while (poweroftwo < value) {
+        poweroftwo <<= 1;
+    }
+    return poweroftwo;
+}
+
+
+void copy_texture_data(void* dest, const void* src, const int pW, const int width, const int height){
+    for (unsigned int y = 0; y < height; y++) {
+        for (unsigned int x = 0; x < width; x++) {
+            ((unsigned int*)dest)[x + y * pW] = ((unsigned int *)src)[x + y * width];
         }
     }
 }
 
-void swizzle_fast(unsigned char* out, const unsigned char*in, const unsigned char *in, const unsigned int width, const unsigned int height)
-{
+typedef struct {
+    unsigned int width, height;
+    unsigned int pW, pH;
+    void* data;
+}Texture;
+
+
+// Create and load a texture
+Texture* load_texture(const char* filename, const int vram) {
+    int width, height, nrChannels;    
+    stbi_set_flip_vertically_on_load(GL_TRUE);
+    unsigned char *data = stbi_load(filename, &width, &height,
+                                    &nrChannels, STBI_rgb_alpha);
+
+    if(!data)
+        return NULL;
+
+    Texture* tex = (Texture*)malloc(sizeof(Texture));
+    tex->width = width;
+    tex->height = height;
+    tex->pW = pow2(width);
+    tex->pH = pow2(height);
+
+    unsigned int *dataBuffer =
+        (unsigned int *)memalign(16, tex->pH * tex->pW * 4);
+
+    // Copy to Data Buffer
+    copy_texture_data(dataBuffer, data, tex->pW, tex->width, tex->height);
+
+    // Free STB Data
+    stbi_image_free(data);
+
+    unsigned int* swizzled_pixels = NULL;
+    size_t size = tex->pH * tex->pW * 4;
+    if(vram){
+        swizzled_pixels = getStaticVramTexture(tex->pW, tex->pH, GU_PSM_8888);
+    } else {
+        swizzled_pixels = (unsigned int *)memalign(16, size);
+    }
     
+    swizzle_fast((u8*)swizzled_pixels, (const u8*)dataBuffer, tex->pW * 4, tex->pH);
+
+    free(dataBuffer);
+    tex->data = swizzled_pixels;
+
+    sceKernelDcacheWritebackInvalidateAll();
+
+    return tex;
+}
+
+// Tells to the GPU "hey, i have a texture"
+void bind_texture(Texture* tex) {
+    if(tex == NULL)
+        return;
+
+    glTexMode(GU_PSM_8888, 0, 0, 1);
+    glTexFunc(GL_TFX_MODULATE, GL_TCC_RGBA);
+    glTexFilter(GL_NEAREST, GL_NEAREST);
+    glTexWrap(GL_REPEAT, GL_REPEAT);
+    glTexImage(0, tex->pW, tex->pH, tex->pW, tex->data);
 }
 
 int main() {
@@ -127,6 +224,8 @@ int main() {
     glMatrixMode(GL_MODEL);
     glLoadIdentity(); // Load Matrix and default values
 
+    Texture* tex = load_texture("resources/sample.jpg", GL_TRUE);
+
     //Main program loop
     while(running){
         //startFrame();
@@ -136,7 +235,7 @@ int main() {
         glDisable(GL_DEPTH_TEST);
         //glDisable(GL_TEXTURE_2D);
 
-        //Clear background to Green
+        //Clear background to black
         glClearColor(0xFF000000);
         glClearColor(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
@@ -148,6 +247,7 @@ int main() {
         //Move back to origin, and down 0.5f
         reset_transform(0.0f, -0.5f, 0.0f);
         // Draw Indexed Square
+        bind_texture(tex);
         glDrawElements(GL_TRIANGLES, GL_INDEX_16BIT | GL_TEXTURE_32BITF | GL_VERTEX_32BITF | GL_COLOR_8888 | GL_TRANSFORM_3D, 6, indices, square_indexed);
 
         //endFrame();
