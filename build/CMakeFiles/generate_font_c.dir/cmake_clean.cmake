018c156e82be0afcf1723b86304850f5f60e565f
Caio Madeira <caiomadeira.dev@gmail.com>
2025-06-21 20:20:13 -0300

    rm old from main

--- build/CMakeFiles/generate_font_c.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "/home/caiomadeira/Desktop/pspsdk-samples/src/font.c"
-  "CMakeFiles/generate_font_c"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/generate_font_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
